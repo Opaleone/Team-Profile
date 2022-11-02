f083cce9c43aca33a541d2ceb675dba967551930
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-11-02 17:56:26 -0500

    Completed functions to write to html

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,7 @@
+const Manager = require("../lib/manager");
+const Engineer = require('../lib/engineer')
+const Intern = require('../lib/intern')
+
 function generateHtml(data) {
   return `<!DOCTYPE html>
 <html lang="en">
@@ -25,59 +29,67 @@ function generateHtml(data) {
 }
 
 function generateEmployeeCards(data) {
+  let employeeCards = '';
+
   for (let i = 0; i < data.newEmployee.length; i++) {
     if (data.newEmployee[i].employeeChoice === 'Engineer') {
-      return generateEngineer(data);
+      employeeCards += generateEngineer(data.newEmployee[i]);
     } else if (data.newEmployee[i].employeeChoice === 'Intern') {
-      return generateIntern(data);
-    } else {
-      return '';
-    }
+      employeeCards += generateIntern(data.newEmployee[i]);
+    } 
   }
+
+  return employeeCards;
 }
 
 function generateInitialCard(data) {
+  let manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNumber)
+
   return `<div class="card col-12 col-md-4 col-lg-4 custom-card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${data.managerName}</h5>
-    <p class="card-text">Manager</p>
+    <h5 class="card-title">${manager.getName()}</h5>
+    <p class="card-text">${manager.getRole()}</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${data.managerId}</li>
-    <li class="list-group-item">Office Number: ${data.managerOfficeNumber}</li>
+    <li class="list-group-item">ID: ${manager.getId()}</li>
+    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
   </ul>
 
-  <a href=mailto:"${data.managerEmail}" class="card-link custom-email">${data.managerEmail}</a>
+  <a href=mailto:"${manager.getEmail()}" class="card-link custom-email">${manager.getEmail()}</a>
 </div>`
 }
 
 function generateEngineer(data) {
+  let engineer = new Engineer(data.employeeName, data.employeeId, data.employeeEmail, data.engineersGithub)
+
   return `<div class="card col-12 col-md-4 col-lg-4 custom-card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${data.newEmployee[0].employeeName}</h5>
-    <p class="card-text">${data.newEmployee[0].employeeChoice}</p>
+    <h5 class="card-title">${engineer.getName()}</h5>
+    <p class="card-text">${engineer.getRole()}</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${data.newEmployee[0].employeeId}</li>
-    <li class="list-group-item">GitHub: <a href="https://github.com/${data.newEmployee[0].engineersGithub}" class="card-link github-link">${data.newEmployee[0].engineersGithub}</a></li>
+    <li class="list-group-item">ID: ${engineer.getId()}</li>
+    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" class="card-link github-link">${engineer.getGithub()}</a></li>
   </ul>
 
-  <a href=mailto:"${data.newEmployee[0].employeeEmail}" class="card-link custom-email">${data.newEmployee[0].employeeEmail}</a>
+  <a href=mailto:"${engineer.getEmail()}" class="card-link custom-email">${engineer.getEmail()}</a>
 </div>`
 }
 
 function generateIntern(data) {
+  let intern = new Intern(data.employeeName, data.employeeId, data.employeeEmail, data.internSchool)
+
   return `<div class="card col-12 col-md-4 col-lg-4 custom-card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${data.newEmployee[0].employeeName}</h5>
-    <p class="card-text">${data.newEmployee[0].employeeChoice}</p>
+    <h5 class="card-title">${intern.getName()}</h5>
+    <p class="card-text">${intern.getRole()}</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${data.newEmployee[0].employeeId}</li>
-    <li class="list-group-item">School: ${data.newEmployee[0].internSchool}</li>
+    <li class="list-group-item">ID: ${intern.getId()}</li>
+    <li class="list-group-item">School: ${intern.getSchool()}</li>
   </ul>
 
-  <a href=mailto:"${data.newEmployee[0].employeeEmail}" class="card-link custom-email">${data.newEmployee[0].employeeEmail}</a>
+  <a href=mailto:"${intern.getEmail()}" class="card-link custom-email">${intern.getEmail()}</a>
 </div>`
 }
 
